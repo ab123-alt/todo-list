8223915ad5f9cea9f4974fab5abeed1d5dd67750
busa <agnes.busa@adesso.de>
2020-10-27 08:52:51 +0100

    adds posting to a differnt list

--- app.js
+++ app.js
@@ -7,21 +7,32 @@ const app = express();
 app.set("view engine", "ejs");
 
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 var items = ["Buy food", "Cook food", "Eat food"];
+var workItems = [];
 
 app.get("/", (req, res) => {
   const currentDay = new Date();
   options = { weekday: "long", day: "numeric", month: "long" };
   var day = currentDay.toLocaleDateString("de-DE", options);
-  res.render("list", { kindOfDay: day, newItems: items });
+  res.render("list", { listTitle: day, newItems: items });
+});
+
+app.get("/work", (req, res) => {
+  res.render("list", { listTitle: "Work list", newItems: workItems });
 });
 
 app.post("/", (req, res) => {
+  console.log(req.body);
   item = req.body.newItem;
-  items.push(item);
-  res.redirect("/");
+  if (req.body.list === "Work") {
+    workItems.push(item);
+    res.redirect("/work");
+  } else {
+    items.push(item);
+    res.redirect("/");
+  }
 });
 
 app.listen(3000, () => {
