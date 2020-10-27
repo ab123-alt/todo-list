5039e1fed8d80832c185e92796252b362368941b
busa <agnes.busa@adesso.de>
2020-10-27 07:07:51 +0100

    adds scripting, adds date formatting

--- app.js
+++ app.js
@@ -6,36 +6,9 @@ const app = express();
 app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
-  const currentDay = new Date().getDay();
-  var day = "";
-
-  switch (currentDay) {
-    case 0:
-      day = "Sunday";
-      break;
-    case 1:
-      day = "Monday";
-      break;
-    case 2:
-      day = "Tuesday";
-      break;
-    case 3:
-      day = "Wednesday";
-      break;
-    case 4:
-      day = "Thursday";
-      break;
-    case 5:
-      day = "Friday";
-      break;
-    case 6:
-      day = "Saturday";
-      break;
-
-    default:
-        console.log('currentDay is equal to', currentDay);
-      break;
-  }
+  const currentDay = new Date();
+  options = {weekday: 'long', day: 'numeric', month: 'long'};
+  var day = currentDay.toLocaleDateString('de-DE', options);
   res.render("list", { kindOfDay: day });
 });
 
