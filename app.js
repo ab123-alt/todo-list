c1d0b7ea337e0d27b0326a511cefddb0c15cdf65
busa <agnes.busa@adesso.de>
2020-10-27 07:28:51 +0100

    adds posting to list, uses for loop in template

--- app.js
+++ app.js
@@ -3,13 +3,24 @@ const bodyParser = require("body-parser");
 const ejs = require("ejs");
 
 const app = express();
+
 app.set("view engine", "ejs");
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+var items = ["Buy food", "Cook food", "Eat food"];
 
 app.get("/", (req, res) => {
   const currentDay = new Date();
-  options = {weekday: 'long', day: 'numeric', month: 'long'};
-  var day = currentDay.toLocaleDateString('de-DE', options);
-  res.render("list", { kindOfDay: day });
+  options = { weekday: "long", day: "numeric", month: "long" };
+  var day = currentDay.toLocaleDateString("de-DE", options);
+  res.render("list", { kindOfDay: day, newItems: items });
+});
+
+app.post("/", (req, res) => {
+  item = req.body.newItem;
+  items.push(item);
+  res.redirect("/");
 });
 
 app.listen(3000, () => {
